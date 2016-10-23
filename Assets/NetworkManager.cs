ab202c76626c5007148dcd0619e7aa9712d3f4b6
Ben <ben@bromleycroft.co.uk>
2016-10-23 13:34:40 +0100

    Upload files to Github

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkManager: MonoBehaviour {
+	void Start() {
+		StartCoroutine(GetText());
+
+	}
+
+	public string ImagePath = "";
+	IEnumerator GetText() {
+		while (true) {
+			UnityWebRequest www = UnityWebRequest.Get ("http://localhost:8000/text.txt");
+			yield return www.Send ();
+
+			if (www.isError) {
+				Debug.Log (www.error);
+			} else {
+				// Show results as text
+				ImagePath = (www.downloadHandler.text);
+				Debug.Log (ImagePath);
+				// Or retrieve results as binary data
+				byte[] results = www.downloadHandler.data;
+			}
+			yield return new WaitForSeconds (5);
+		}
+	}
+		
+}
---
+++ Assets/NetworkTexturing.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkTexturing: MonoBehaviour {
+
+
+	void Start() {
+
+		StartCoroutine(GetTexture());
+	}
+
+	IEnumerator GetTexture() {
+		while (true) {
+			UnityWebRequest WWW = UnityWebRequest.GetTexture (ImagePath);
+			yield return www.Send ();
+
+			if (www.isError) {
+				Debug.Log (www.error);
+			} else {
+				Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+				this.GetComponent <MeshRenderer> ().sharedMaterial.SetTexture ("_MainTex", myTexture);
+			}
+		}
+	}
+}
